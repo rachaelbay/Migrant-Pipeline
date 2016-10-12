80af9acbbba76c820831c442ef726865c87cf139
Rachael Bay <rachaelbay@gmail.com>
2016-10-12 16:19:49 -0700

    Update PCA.R

--- scripts/PCA.R
+++ scripts/PCA.R
@@ -1,4 +1,4 @@
-library(WGCNA)
+library(WGCNA) ##This package needs to be downloaded from bioconductor
 library(data.table)
 library(RColorBrewer)
 
@@ -11,23 +11,15 @@ setwd("~/Documents/DIRECTORY")
 
 meta <- read.delim("meta.txt",header=T)
 data <- data.frame(fread("SNP_MATRIX.012",na.strings="-1",header=F),row.names=1)
-inds <- read.delim("AMKE1_filtered.012.indv",header=F)
+inds <- read.delim("SNP_INDIVIDUALS.012.indv",header=F)
 rownames(data) <- inds[,1]
 snps <- read.delim("SNP_POSITIONS.012.pos",header=F)
-
-##Filter out individuals with low coverage
-##This will eliminate individuals with less than some proportion (covThresh) of SNPs genotyped
-covThresh = 0.25
-goodGT <- apply(data,1,function(x) length(na.omit(x)))
-goodGT <- goodGT/ncol(data)
-filtered <- data[goodGT>covThresh,]
-dim(filtered)
-ordermeta <- meta[match(rownames(filtered),meta$Sample),]
+ordermeta <- meta[match(rownames(data),meta$Sample),]
 
 ###Filter uninformative SNPs
 ##Filters SNPs with only one genotype present
-genNum <- apply(filtered,2,function(x) length(table(x)))
-informative <- filtered[,genNum>1]
+genNum <- apply(data,2,function(x) length(table(x)))
+informative <- data[,genNum>1]
 dim(informative)
 
 ###OPTIONAL: Filter out "bad" individuals: These may have been outliers or samples you think there was a problem with. You can also use the last two lines to filter out potential migrants as long as that is marked in the metadata
@@ -39,17 +31,21 @@ dim(informative)
 # informative <- informative[ordermeta$Group!="migrant",]
 # ordermeta <- ordermeta[ordermeta$Group!="migrant",]
 
+##Fill in missing values with median genotype for that SNP
+for (i in 1:ncol(informative)) {
+	print (i)
+	snp <- informative[,i]
+	snp[is.na(snp)] <- median(snp,na.rm=T)
+	informative[,i] <- snp
+}
 
 
 ####PCA
-noNA <- na.omit(t(informative))
-dim(noNA) ##This is how many SNPs can be used for the PCA
-pca <- prcomp(t(noNA))
-colors <- brewer.pal(12,"Set3")[c(1:8,10:12)] ##You will need a different color pallette if you need >12 colors
+pca <- prcomp(informative)
+colors <- c(brewer.pal(12,"Set3"),brewer.pal(8,"Set1"),brewer.pal(8,"Set2"))
 par(mfrow=c(1,2),tck=0.025,mar=c(4,4,1,1),mgp=c(1.5,0.1,0))
 plot(pca$x[,1],pca$x[,2],pch=19,col=labels2colors(ordermeta$State,colorSeq=colors),
      cex=1.5,cex.lab=1.7,xlab="PC1",ylab="PC2")
 plot(1:10,1:10,type="n",axes=F,xlab="",ylab="")
 legend("left",bty="n",cex=1.2,legend=unique(ordermeta$State),fill=unique(labels2colors(ordermeta$State,colorSeq=colors)))
-
 
